8a45e6010a4a77a99b758d234e818a630f8670bc
Quincy Thai <quincythai50@icloud.com>
2024-03-29 12:51:35 -0700

    Add theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
-// pages/_app.js
+// pages/_app.tsx
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "@/styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
