3f20bae1043d1d54142d2958ad292cf4779f1b2c
Quincy Thai <quincythai50@icloud.com>
2024-04-07 14:12:41 -0700

    Setup createdoc for postman

--- src/pages/api/createDoc.ts
+++ src/pages/api/createDoc.ts
@@ -1,29 +1,38 @@
-// pages/api/createDoc.ts
+import { MongoClient } from 'mongodb';
+// Interface: can be extended upon by classes
+// Type: cannot be extended by classes; preferred when need to use unions or tuples or define type in a way that does not fit the structure of an object
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { NextApiRequest, NextApiResponse } from "next";
-import clientPromise from "../../lib/mongodb";
+// Connect to the database
+// Only runs when we call it in handler
+async function connectToDatabase() {
+  const client = new MongoClient(process.env.MONGODB_URI!);
+  await client.connect();
+  return client;
+}
 
-export default async function createDoc(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
+// runs whenever localhost:3000/api/createDoc is called
+// capture request in req, result in res
+// default: allows next.js to find it
+// async: allows await to wait for promises and not block thread
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
     try {
-      const client = await clientPromise;
-      const db = client.db("your_db_name"); // Replace with your DB name
+      const client = await connectToDatabase();
+      // client.db('database_name_here')
+      const db = client.db('notiom');
+      
+      // db.collection('collection_name_here')
+      const result = await db.collection('documents').insertOne(req.body);
 
-      // The document to insert
-      const doc = req.body;
-
-      // Insert the document into the collection
-      const result = await db.collection("your_collection_name").insertOne(doc);
-
+      // 201: Success: Created
       res.status(201).json(result);
-    } catch (e) {
-      res.status(500).json({ error: e.message });
+    } catch (error) {
+      // 500: Server error - internal server error
+      res.status(500).json({ error: 'Unable to create document' });
     }
   } else {
-    // Handle any other HTTP method
-    res.status(405).json({ error: "Method Not Allowed" });
+    // 405: Client error - Method not allowed (i.e. not supposed to be here)
+    res.status(405).json({ error: 'Method not allowed' });
   }
 }
